40ddd4d7a49e359853959cc9ff5d1bf85272d8fd
Carlos Florian <cmflorian@msn.com>
2022-08-28 16:37:24 -0400

    08-21-22 Meet

---
+++ macro.py
@@ -0,0 +1,28 @@
+##keyboard defintions
+
+
+##MACROS##
+#drink - define drink key
+def drink():
+	print("drinking")
+
+#eat - define eat key
+def eat():
+	print("eating")
+
+#bandage - define bandage key
+def bandage():
+	print("bandage")
+
+#potion - define potion key
+def potion():
+	print("potion")
+
+#elixir - define elixir key
+def elixir():
+	print("elixir")
+
+#BOOFS so we can BOOF while we BOOF!!!!!!
+def BOOFS():
+	print("Ramirez!! DO EVERYTHING")
+	print("Ramirez!! LAST BOOF MAKE IT COUNT")
