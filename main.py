df83c586157ca437659b8c5ffd5c2b0e3c931672
Carlos Florian <cmflorian@msn.com>
2022-08-28 16:38:58 -0400

    08-21-22 meet

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-from pyautogui import *
-import pyautogui
-import time
-import keyboard
-import random
-import win32api, win32con
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    
 
 
 
 #help
+
+
+#ToDo List:
+
+#get bot to interface with the game.
+#create folder in wow -> interface -> addons
+
+#Select between the different party members:
+#	F1-F5
+
+# Use healing abilities
+#	Priest spams Flash of Light and or higher ranking spells
+
+#Be able to check other party members to see if they need healing
+#	make sure the bot can follow us
+#		eventually make the bot smart enough to walk to other party members to be in range of healing.
+
+#Logic for damage over time and threat to differentiate different spells to use.
+
+import macro
+import time
+import pyautogui
+import keyboard
+import random
+
+###  MOVEMENT  ###
+
+""" WoW DEFAULTS
+
+Move and Steer: Middle Mouse
+Move Forward: W
+Move Backward: S
+Turn Left: A
+Turn Right: D
+Strafe Left: Q
+Strafe Right: E
+Jump: Spacebar
+Sit/Move Down: X
+Sheath/Unsheath: Z
+Toggle Autorun: Num Lock
+Toggle Run/Walk: Num Pad/
+"""
+
+def Test():
+	time.sleep(1.5)
+	pyautogui.press('w')
+
+Test()
